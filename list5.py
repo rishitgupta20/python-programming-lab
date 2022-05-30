3bc74eb6541a7d98b8357606403ce367fe18435b
Rishit Gupta <92114513+rishitgupta20@users.noreply.github.com>
2022-05-30 14:18:49 +0530

    Add files via upload

---
+++ list5.py
@@ -0,0 +1,6 @@
+a=input("enter the string")
+b=a[::-1]
+if(a==b):
+    print("string is palindrom")
+else:
+    print("not palindrom")
---
+++ str4.py
@@ -0,0 +1,7 @@
+from numpy import right_shift
+
+
+a = input("enter the first name")
+b=input("enter the surname")
+c=a+ b
+print("the name in reverse wise =",c[::-1])
